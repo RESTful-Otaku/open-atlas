6070c70423faa46f3eb80039705de3812dda80fd
restful-otaku <j-m-harrison@protonmail.com>
2026-06-25 13:51:41 +0100

    cleanup: remove dead barrel file viz/index.ts (no consumers)

--- web/src/lib/viz/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as EChartsPanel } from "./EChartsPanel.svelte";
-export { default as FullscreenChartShell } from "./FullscreenChartShell.svelte";
-export { default as GeoVizMap } from "./GeoVizMap.svelte";
-export * from "./chart-theme";
-export * from "./showcase-datasets";
-export * from "./showcase-options";
