109f5b549c9b5fca9cc974f9611c2100e5ceadaa
restful-otaku <j-m-harrison@protonmail.com>
2026-06-26 10:59:00 +0100

    cleanup: inline narrative-subscription.ts into its two consumers
    - Remove narrative-subscription.ts (10-line wrapper around onMount + acquireNarrativeSubscription)
    - Inline onMount(() => acquireNarrativeSubscription()) directly in EventDetailView.svelte and HubView.svelte

--- web/src/lib/narrative-subscription.ts
+++
@@ -1,8 +0,0 @@
-
-import { onMount } from "svelte";
-import { acquireNarrativeSubscription } from "./connection.svelte";
-
-
-export function useNarrativeSubscription(): void {
-  onMount(() => acquireNarrativeSubscription());
-}
